967c46477d44830d93fb6595694be6e17d100377
LilOmelette <9469301+LilOmelette@users.noreply.github.com>
2026-02-04 14:40:17 -0500

    Added score tracker to high scores table

--- Zoomboni/Assets/Scripts/MainMenu.cs
+++ Zoomboni/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public TextMeshProUGUI highScoreOneText;
+    public TextMeshProUGUI highScoreTwoText;
+    public TextMeshProUGUI highScoreThreeText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       highScoreOneText.text = PlayerPrefs.GetFloat("SCORE_1").ToString("0"); 
+       highScoreTwoText.text = PlayerPrefs.GetFloat("SCORE_2").ToString("0"); 
+       highScoreThreeText.text = PlayerPrefs.GetFloat("SCORE_3").ToString("0"); 
     }
 
     // Function to start the game when users press the start button
