4b00edba43583508ddfd06cc56ed3582a18886cd
Ganonbread <richiehull96@gmail.com>
2026-02-06 16:05:54 -0500

    mama bear goes Woohoohooooooo when she throws you now.
    Oh, and the throw animation is faster, so you can see it happen

--- Zoomboni/Assets/Scripts/Zones/BouncyMamaBear.cs
+++ Zoomboni/Assets/Scripts/Zones/BouncyMamaBear.cs
@@ -4,8 +4,27 @@ public class BouncyMamaBear : Bouncy
 {
 
     public Animator animator;
+    public AudioSource audio;
+    public float animationSpeed;
+    public Timer animationDelay;
+
+
+
+    public void Update()
+    {
+        if (!animationDelay.IsActive()){
+            animator.speed = 1;
+        }
+    }
+
     protected override void OnTrigger()
     {
+        animator.speed = animationSpeed;
         animator.Play("Armature|throw");
+        audio.Play();
+        animationDelay.Reset();
     }
+    
+    
+
 }
